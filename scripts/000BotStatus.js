919ebca99c3a343604178dd5d444ed1b1b52c5b3
palladas <georkats8@students.kastoria.teikoz.gr>
2018-07-11 00:15:27 +0300

    Added Watchdog for 000bot

---
+++ scripts/000BotStatus.js
@@ -0,0 +1,24 @@
+module.exports = function(robot){
+    //set as constants the bot's discord ids room ids and user ids
+    const roomID = "372833397346664451";
+    const userID = "227833123897868289";
+    const devBot = "436971323319779331";
+    //client emits a presenceUpdate event. Check who is the user
+    robot.client.on("presenceUpdate", (userBefore, userAfter) => {
+       if (userAfter.id === devBot)
+        {
+            if (userAfter.presence.status === 'offline')
+            {
+            //sent message to room and user Palladas. 
+            robot.messageRoom(roomID, "TripleZeroBot-Development went offline");
+            robot.messageRoom(userID, "TripleZeroBot-Development went offline");
+            }
+            else if (userAfter.presence.status === 'online')
+            {
+            //sent message to room and user Palladas
+            robot.messageRoom(roomID, "TripleZeroBot-Development Is back on line");
+            robot.messageRoom(userID, "TripleZeroBot-Development Is back on line");
+            }
+        }
+    });
+};
